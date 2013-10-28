8763614775e10e56b91b38dd2f689a426060a6f1
adefran83 <adefran83@gmail.com>
2013-10-27 21:56:44 -0400

    chapter 6

---
+++ Sample/Demos/CalculatorDemo.aspx.cs
@@ -0,0 +1,46 @@
+﻿using Sample.App_Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sample.Demos
+{
+    public partial class CalculatorDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CalculateButton_Click(object sender, EventArgs e)
+        {
+            if(ValueBox1.Text.Length > 0 && ValueBox2.Text.Length > 0)
+            {
+                double result = 0;
+                double value1 = Convert.ToDouble(ValueBox1.Text);
+                double value2 = Convert.ToDouble(ValueBox2.Text);
+
+                Calculator calc = new Calculator(); 
+                switch (OperatorList.SelectedValue)
+                {
+                    case "+":
+                        result = calc.Add(value1, value2);
+                        break;
+                    case "-":
+                        result = calc.Subtract(value1, value2);
+                        break;
+                    case "*":
+                        result = calc.Multiply(value1, value2);
+                        break;
+                    case "/":
+                        result = calc.Divide(value1, value2);
+                        break;
+                }
+                ResultLabel.Text = result.ToString();
+            }
+        }
+    }
+}
--- Sample/Login.aspx.designer.cs
+++ Sample/Login.aspx.designer.cs
@@ -11,7 +11,7 @@ namespace Sample
 {
 
 
-    public partial class Default
+    public partial class Login
     {
     }
 }
